d01fe3fb605955b227de432031eddb35a135cb8e
ivankoi <ivankoi@gmail.com>
2017-01-22 23:39:08 +0100

    Added Race entity

--- src/main/java/com/ivankoi/study/spring/bussines/team/entity/Race.java
+++ src/main/java/com/ivankoi/study/spring/bussines/team/entity/Race.java
@@ -4,4 +4,25 @@ package com.ivankoi.study.spring.bussines.team.entity;
  * Created by ivank on 1/22/2017.
  */
 public class Race {
+
+    private String code;
+    private String description;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
 }
---
+++ src/test/java/com/ivankoi/study/spring/bussines/team/boundry/TeamServiceTest.java
@@ -0,0 +1,16 @@
+package com.ivankoi.study.spring.bussines.team.boundry;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ivank on 1/22/2017.
+ */
+public class TeamServiceTest {
+    @Test
+    public void findAllRaces() throws Exception {
+
+    }
+
+}
